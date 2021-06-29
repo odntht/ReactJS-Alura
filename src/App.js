c2e215811625fd25d0dc13882b72b485a8528209
Fernando <odntht@gmail.com>
2021-06-29 11:04:06 -0300

    deleting cards

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-import React, { Component } from "react";
-import ListaDeNotas from "./components/ListaDeNotas";
-import FormularioCadastro from "./components/FormularioCadastro";
-import "./assets/App.css";
+import React, { Component } from 'react';
+import ListaDeNotas from './components/ListaDeNotas';
+import FormularioCadastro from './components/FormularioCadastro';
+import './assets/App.css';
 import './assets/index.css';
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      notas:[]
-    }
+      notas: [],
+    };
   }
 
-  criarNota(titulo, texto){
-    const novaNota = {titulo, texto};
-    const novoArrayNotas = [...this.state.notas,novaNota]
+  criarNota(titulo, texto) {
+    const novaNota = { titulo, texto };
+    const novoArrayNotas = [...this.state.notas, novaNota];
     const novoEstado = {
-      notas:novoArrayNotas
-    }
-    this.setState(novoEstado)
+      notas: novoArrayNotas,
+    };
+    this.setState(novoEstado);
+  }
+
+  deletarNota(index) {
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index, 1);
+    this.setState({ notas: arrayNotas });
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.state.notas}/>
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas
+          notas={this.state.notas}
+          apagarNota={this.deletarNota.bind(this)}
+        />
       </section>
     );
   }
