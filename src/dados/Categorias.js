a3d83478b78c9a0ccf6e96d53c8b01dfdf0cef4f
Fernando <odntht@gmail.com>
2021-07-02 17:00:28 -0300

    finalizando aula 4 observables

--- src/dados/Categorias.js
+++ src/dados/Categorias.js
@@ -1,9 +1,18 @@
 export default class Categorias {
-  constructor(){
-    this.categorias = []
+  constructor() {
+    this.categorias = [];
+    this._inscritos = [];
   }
 
-  adicionarCategoria(novaCategoria){
-    this.categorias.push(novaCategoria)
+  inscrever(func) {
+    this._inscritos.push(func);
+  }
+
+  notificar() {
+    this._inscritos.forEach((func) => func(this.categorias));
+  }
+
+  adicionarCategoria(novaCategoria) {
+    this.categorias.push(novaCategoria);
   }
 }
--- src/dados/Notas.js
+++ src/dados/Notas.js
@@ -1,22 +1,31 @@
-export default class ArrayDeNotas{
-  constructor(){
-    this.notas = []
-  }  
-
-  adicionarNota(titulo, texto, categoria){
-    const novaNota = new Nota(titulo, texto, categoria);
-    this.notas.push(novaNota)
+export default class ArrayDeNotas {
+  constructor() {
+    this.notas = [];
+    this._inscritos = [];
   }
 
-  apagarNotas(index){
-    this.notas.splice(index,1)
+  adicionarNota(titulo, texto, categoria) {
+    const novaNota = new Nota(titulo, texto, categoria);
+    this.notas.push(novaNota);
+  }
+
+  apagarNotas(index) {
+    this.notas.splice(index, 1);
+  }
+
+  inscrever(func) {
+    this._inscritos.push(func);
+  }
+
+  notificar() {
+    this._inscritos.forEach((func) => func(this.categorias));
   }
 }
 
 class Nota {
-  constructor(titulo, texto, categoria){
+  constructor(titulo, texto, categoria) {
     this.titulo = titulo;
     this.texto = texto;
-    this.categoria = categoria
+    this.categoria = categoria;
   }
 }
